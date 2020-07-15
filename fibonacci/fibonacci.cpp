35fb9a5b306f98dba39409d9a674bc6b4148d1f6
Andrew Shitov <andy@shitov.ru>
2020-07-15 17:30:50 +0200

    Simplify C++ Fib

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 unsigned int fib(unsigned int n) {
-    if (!n) return 0;
-    if (n < 2) return 1;
+    if (n < 2) return n;
 
     return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
-    for (int n = 0; n <= 100; n++) {
+    for (int n = 0; n <= 30; n++) {
         cout << "fib(" << n << ") = " << fib(n) << endl;
     }
 }
